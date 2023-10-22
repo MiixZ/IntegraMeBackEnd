972c9c043fc34c40118bec41953503c084ea5e99
MiixZ <xwarzmiixz@gmail.com>
2023-10-22 12:27:50 +0200

    prueba1

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 
-import { createServer } from 'http';
+//import { createServer } from 'http';
 
 const app = express();
 app.use(express.json());
+
+app.listen(8080, () => {
+    console.log('Server escuchando en puerto 8080');
+});
