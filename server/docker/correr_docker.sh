c825ae2ef25b68f940d42a9e998c20ed12e0a370
Ismael Díaz <xWaRzMiixZ@gmail.com>
2023-11-20 12:46:38 +0100

    CAMBIO PUERTO

--- server/docker/correr_docker.sh
+++ server/docker/correr_docker.sh
@@ -10,4 +10,4 @@ else
 fi
 
 docker build . -t integrame/nodejs
-docker run -p 30000:8080 --name nodejs-integrame -d integrame/nodejs
+docker run -p 6969:6969 --name nodejs-integrame -d integrame/nodejs
