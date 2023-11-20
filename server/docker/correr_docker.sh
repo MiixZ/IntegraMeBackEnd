265ad1e0153f2b28abb6658c38c3c0f87ba18195
Ismael Díaz <xWaRzMiixZ@gmail.com>
2023-11-20 13:06:29 +0100

    Script server docker CHANGE: Modificado el script para cuando se haga un pull en la VM.

--- server/docker/correr_docker.sh
+++ server/docker/correr_docker.sh
@@ -1,13 +1,5 @@
-echo IMAGES_PATH=$(pwd)/../images/
-
-IMAGES_PATH=$(pwd)/../images/
-if grep -q "IMAGES_PATH=" ../.env; then
-  # Si la línea existe, reemplázala
-  sed -i "s|^IMAGES_PATH=.*|IMAGES_PATH=$IMAGES_PATH|" ../.env
-else
-  # Si la línea no existe, agrégala
-  echo "IMAGES_PATH=$IMAGES_PATH" >> ../.env
-fi
+sudo rm -rf node_modules/
+npm install
 
 docker build . -t integrame/nodejs
 docker run -p 6969:6969 --name nodejs-integrame -d integrame/nodejs
